ed8995ba51fb784db31a0e88fcf3c43e5ba571a1
czc <2072480307@qq.com>
2022-06-29 23:13:21 +0800

    add sqlite qml interaction, can bluid songlist and read from sqlite

--- main_zh_CN.ts
+++ main_zh_CN.ts
@@ -5,15 +5,15 @@
     <name>CurrentList</name>
     <message>
         <source>pause</source>
-        <translation type="vanished">暂停</translation>
+        <translation>暂停</translation>
     </message>
     <message>
         <source>play</source>
-        <translation type="vanished">播放</translation>
+        <translation>播放</translation>
     </message>
     <message>
         <source>like</source>
-        <translation type="vanished">喜欢</translation>
+        <translation>喜欢</translation>
     </message>
     <message>
         <source>Create List</source>
@@ -23,26 +23,38 @@
         <source>List Name</source>
         <translation type="vanished">歌单名</translation>
     </message>
+    <message>
+        <source>add list</source>
+        <translation>添加歌单</translation>
+    </message>
 </context>
 <context>
     <name>DesktopLrc</name>
     <message>
         <source>No Lyrics</source>
-        <translation type="vanished">暂无歌词</translation>
+        <translation>暂无歌词</translation>
     </message>
 </context>
 <context>
     <name>FileD</name>
     <message>
         <source>Please choose an image file</source>
-        <translation type="vanished">请选择一个图片文件</translation>
+        <translation>请选择一个图片文件</translation>
     </message>
 </context>
 <context>
     <name>Footerwindow</name>
     <message>
         <source>词</source>
-        <translation type="vanished">词</translation>
+        <translation>词</translation>
+    </message>
+    <message>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source></source>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -68,212 +80,243 @@
     <name>LocaMusicPage</name>
     <message>
         <source>Local music</source>
-        <translation type="vanished">本地音乐</translation>
+        <translation>本地音乐</translation>
     </message>
     <message>
         <source>Song Number:</source>
-        <translation type="vanished">音乐数：</translation>
+        <translation>音乐数：</translation>
     </message>
     <message>
         <source>directory</source>
-        <translation type="vanished">选择目录</translation>
+        <translation>选择目录</translation>
     </message>
     <message>
         <source>music</source>
-        <translation type="vanished">音乐</translation>
+        <translation>音乐</translation>
     </message>
     <message>
         <source>folder</source>
-        <translation type="vanished">选择文件夹</translation>
+        <translation>选择文件夹</translation>
     </message>
     <message>
         <source>local music</source>
-        <translation type="vanished">本地音乐</translation>
+        <translation>本地音乐</translation>
     </message>
     <message>
         <source>level up</source>
-        <translation type="vanished">音质升级</translation>
+        <translation>音质升级</translation>
     </message>
 </context>
 <context>
     <name>LyrEditing</name>
     <message>
         <source>&amp;Open</source>
-        <translation type="vanished">打开</translation>
+        <translation>打开</translation>
     </message>
     <message>
         <source>&amp;Save</source>
-        <translation type="vanished">保存</translation>
+        <translation>保存</translation>
     </message>
     <message>
         <source>SaveAs</source>
-        <translation type="vanished">另存为</translation>
+        <translation>另存为</translation>
+    </message>
+    <message>
+        <source>[00:00]</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyLikeList</name>
+    <message>
+        <source>title</source>
+        <translation type="unfinished">标题</translation>
+    </message>
+    <message>
+        <source>singer</source>
+        <translation type="unfinished">歌手</translation>
+    </message>
+    <message>
+        <source>album</source>
+        <translation type="unfinished">专辑</translation>
+    </message>
+    <message>
+        <source>time</source>
+        <translation>时间</translation>
     </message>
 </context>
 <context>
     <name>NavigationBar</name>
     <message>
         <source>recommend</source>
-        <translation type="vanished">推荐</translation>
+        <translation>推荐</translation>
     </message>
     <message>
         <source>Search New Song</source>
-        <translation type="vanished">发现音乐</translation>
+        <translation>发现音乐</translation>
     </message>
     <message>
         <source>MV</source>
-        <translation type="vanished">MV</translation>
+        <translation>MV</translation>
     </message>
     <message>
         <source>Friend</source>
-        <translation type="vanished">朋友</translation>
+        <translation>朋友</translation>
     </message>
     <message>
         <source>MySong</source>
-        <translation type="vanished">我的音乐</translation>
+        <translation>我的音乐</translation>
     </message>
     <message>
         <source>Local Music</source>
-        <translation type="vanished">本地音乐</translation>
+        <translation>本地音乐</translation>
     </message>
     <message>
         <source>Download Manage</source>
-        <translation type="vanished">下载管理</translation>
+        <translation>下载管理</translation>
     </message>
     <message>
         <source>Recents</source>
-        <translation type="vanished">最近播放</translation>
+        <translation>最近播放</translation>
     </message>
     <message>
         <source>MySongCloud</source>
-        <translation type="vanished">我的音乐云盘</translation>
+        <translation>我的音乐云盘</translation>
     </message>
     <message>
         <source>EditLyr</source>
-        <translation type="vanished">歌词编辑</translation>
+        <translation>歌词编辑</translation>
     </message>
     <message>
         <source>MyCollection</source>
-        <translation type="vanished">我的收藏</translation>
+        <translation>我的收藏</translation>
     </message>
     <message>
         <source>List</source>
-        <translation type="vanished">歌单</translation>
+        <translation>歌单</translation>
     </message>
     <message>
         <source>LikeMusic</source>
         <translation type="vanished">我喜欢的音乐</translation>
+    </message>
+    <message>
+        <source>Create List</source>
+        <translation type="unfinished">歌单创建</translation>
+    </message>
+    <message>
+        <source>list name</source>
+        <translation type="unfinished">输入歌单名</translation>
     </message>
 </context>
 <context>
     <name>SearchPage</name>
     <message>
         <source>Title</source>
-        <translation type="vanished">标题</translation>
+        <translation>标题</translation>
     </message>
     <message>
         <source>Singer</source>
-        <translation type="vanished">歌手</translation>
+        <translation>歌手</translation>
     </message>
     <message>
         <source>Album</source>
-        <translation type="vanished">专辑</translation>
+        <translation>专辑</translation>
     </message>
     <message>
         <source>Duration</source>
-        <translation type="vanished">时长</translation>
+        <translation>时长</translation>
     </message>
     <message>
         <source>Pause</source>
-        <translation type="vanished">暂停</translation>
+        <translation>暂停</translation>
     </message>
     <message>
         <source>Play</source>
-        <translation type="vanished">播放</translation>
+        <translation>播放</translation>
     </message>
     <message>
         <source>Like</source>
-        <translation type="vanished">喜欢</translation>
+        <translation>喜欢</translation>
     </message>
     <message>
         <source>Download</source>
-        <translation type="vanished">下载</translation>
+        <translation>下载</translation>
     </message>
     <message>
         <source>song</source>
-        <translation type="vanished">歌曲</translation>
+        <translation>歌曲</translation>
     </message>
     <message>
         <source>lyr</source>
-        <translation type="vanished">歌单</translation>
+        <translation>歌单</translation>
     </message>
 </context>
 <context>
     <name>SearchRect</name>
     <message>
         <source>Popular</source>
-        <translation type="vanished">热门搜索</translation>
+        <translation>热门搜索</translation>
     </message>
     <message>
         <source>xusong</source>
-        <translation type="vanished">许嵩</translation>
+        <translation>许嵩</translation>
     </message>
     <message>
         <source>NoReason</source>
-        <translation type="vanished">没有理由</translation>
+        <translation>没有理由</translation>
     </message>
     <message>
         <source>CaiXuKun</source>
-        <translation type="vanished">蔡徐坤</translation>
+        <translation>蔡徐坤</translation>
     </message>
     <message>
         <source>LuoZiXian</source>
-        <translation type="vanished">罗志祥</translation>
+        <translation>罗志祥</translation>
     </message>
     <message>
         <source>JayChou</source>
-        <translation type="vanished">周杰伦</translation>
+        <translation>周杰伦</translation>
     </message>
 </context>
 <context>
     <name>SongList</name>
     <message>
         <source>Like</source>
-        <translation type="vanished">喜欢</translation>
+        <translation>喜欢</translation>
     </message>
     <message>
         <source>Collect</source>
-        <translation type="vanished">收藏</translation>
+        <translation>收藏</translation>
     </message>
     <message>
         <source>Download</source>
-        <translation type="vanished">下载</translation>
+        <translation>下载</translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation type="vanished">歌名：</translation>
+        <translation>歌名：</translation>
     </message>
     <message>
         <source>Album</source>
-        <translation type="vanished">专辑</translation>
+        <translation>专辑</translation>
     </message>
     <message>
         <source>Singer</source>
-        <translation type="vanished">歌手</translation>
+        <translation>歌手</translation>
     </message>
 </context>
 <context>
     <name>TitleBar</name>
     <message>
         <source>Search</source>
-        <translation type="vanished">发现</translation>
+        <translation>发现</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
         <source>List</source>
-        <translation type="vanished">歌单</translation>
+        <translation>歌单</translation>
     </message>
 </context>
 </TS>
